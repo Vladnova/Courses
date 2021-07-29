42ccbad1acf94681d3bc83884822a2268aa502d4
Vladislav <novalenkov@ukr.net>
2021-07-29 22:13:13 +0300

    initial

--- keys/index.js
+++ keys/index.js
@@ -1,9 +1,5 @@
-module.exports = {
-  MONGODB_URL:
-    "mongodb+srv://Vlados:147852369@cluster0.rskwa.mongodb.net/shop?w=majority",
-  SESSION_SECRET: "some secret value",
-  SENDGRID_API_KEY:
-    "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",
-  EMAIL_FROM: "novalenkov05@gmail.com",
-  BASE_URL:"http://localhost:3000",
-};
+if(process.env.NODE_ENV==="production"){
+  module.exports=require('./keys.prod');
+}else{
+  module.exports=require('./keys.dev');
+}
